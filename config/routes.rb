97461fe99d5073e4653f4c7e9ec35fdd92e669a9
Andre Menezes <djdedemenezes@gmail.com>
2022-12-06 13:26:58 -0300

    code from lecture with comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,28 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # CODE IN SILO: route -> controller action -> view
+  # verb '/path', to: 'controller#action', as: 'prefix'
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # READ ALL -> index
+  get '/restaurants', to: 'restaurants#index', as: 'restaurants'
+
+  # ADD A RESTAURANT: 2 actions -> (new / create)
+  # DISPLAY THE FORM -> new
+  get '/restaurants/new', to: 'restaurants#new', as: 'new_restaurant'
+
+  # RECEIVE THE FORM -> create
+  post '/restaurants', to: 'restaurants#create'
+
+  # CHANGE A RESTAURANT: 2 actions -> (edit / update)
+
+  # DISPLAY THE FORM -> edit
+  get '/restaurants/:id/edit', to: 'restaurants#edit', as: 'edit_restaurant'
+
+  # RECEIVE THE FORM -> update
+  patch '/restaurants/:id', to: 'restaurants#update'
+
+  # READ ONE -> show
+  get '/restaurants/:id', to: 'restaurants#show', as: 'restaurant'
+
+  # DELETE RESTAURANT -> destroy
+  delete '/restaurants/:id', to: 'restaurants#destroy'
 end
